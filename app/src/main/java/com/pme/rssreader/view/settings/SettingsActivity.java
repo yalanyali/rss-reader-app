c7e5ea6704035e3f59ba06e1274186179e1b343d
Yigitcan Alanyali <y.alanyali@gmail.com>
2020-07-22 15:51:45 +0200

    Updated Strings, added dark theme.

--- app/src/main/java/com/pme/rssreader/view/settings/SettingsActivity.java
+++ app/src/main/java/com/pme/rssreader/view/settings/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.pme.rssreader.view.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,13 +23,6 @@ public class SettingsActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
 }
--- app/src/main/java/com/pme/rssreader/view/settings/SettingsFragment.java
+++ app/src/main/java/com/pme/rssreader/view/settings/SettingsFragment.java
@@ -1,16 +1,57 @@
 package com.pme.rssreader.view.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.pme.rssreader.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        Log.e("SETTINGS", key);
+        switch (key) {
+            case "darkMode":
+//                Log.e("SETTINGS", String.valueOf(sharedPreferences.getBoolean(key, true)));
+                int nightMode = sharedPreferences.getBoolean(key, false) ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO;
+                AppCompatDelegate.setDefaultNightMode(nightMode);
+                break;
+            case "colorAccent":
+            case "syncInterval":
+                Log.e("SETTINGS", sharedPreferences.getString(key, ""));
+                break;
+            case "sync":
+                Log.e("SETTINGS", String.valueOf(sharedPreferences.getBoolean(key, true)));
+                break;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Register the preferenceChange listener
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Unregister the preference change listener
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 
 }
